76c117a8ab336d54fe5bc8e9f690bf54dfab65f4
Kate Levantsova <kate.levantsova@gmail.com>
2019-09-19 13:55:42 +0300

    Создала компонент фонаря, установила три цвета

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <v-light class="red" v-bind:is-active="statesLights[0]"></v-light>
+    <v-light class="yellow" v-bind:is-active="statesLights[1]"></v-light>
+    <v-light class="green" v-bind:is-active="statesLights[2]"></v-light>
+  </div>
 </template>
 
 <script>
+import VLight from "./components/Signal.vue";
+
 export default {
-  name: "app"
+  name: "app",
+  components: {
+    VLight
+  },
+  data() {
+    return {
+      statesLights: [true, false, false]
+    };
+  }
 };
 </script>
+<style >
+.red {
+  background: red;
+  opacity: 0.2;
+  margin-bottom: 10px;
+}
+.yellow {
+  background: yellow;
+  opacity: 0.2;
+  margin-bottom: 10px;
+}
+.green {
+  background: green;
+  opacity: 0.2;
+}
+</style>
---
+++ src/components/Signal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="traffic-light" :class="{ 'light--active': isActive }"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    isActive: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>>
+
+<style scoped>
+.traffic-light {
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+}
+.light--active {
+  opacity: 1;
+}
+</style>
